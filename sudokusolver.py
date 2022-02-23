4725bbc3660283d62952a7e196fa1a2def5f3a8a
nadiamason <89014605+nadiamason@users.noreply.github.com>
2022-02-23 19:49:56 +0000

    boxes obtained

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,3 +1,13 @@
+def divide_chunks(l, n):
+      
+    # looping till length l
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
+
+def making_grid(l, n):
+    print(l)
+
+
 grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
         [5, 2, 0, 0, 0, 0, 0, 0, 0],
         [0, 8, 7, 0, 0, 0, 0, 3, 1],
@@ -10,9 +20,30 @@ grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
 
 columns = []
 
-column1 = [item[0] for item in grid]
-
 for i in range(9):
     columns.append([item[i] for item in grid])
 
-print(columns)
+# list should have
+n = 3
+boxes = []
+final_boxes = []
+for row in grid:
+
+    x = list(divide_chunks(row, n))
+    boxes.append(x)
+
+
+for i in range(3):
+    f_boxes = []
+
+    for y in boxes:
+        f_boxes.append(y[i])
+    
+    final_boxes.append(f_boxes)
+
+the_boxes = []
+for box in final_boxes:
+    z = list(divide_chunks(box, n))
+    for i in range(0,3):
+        z[i] = [x for l in z[i] for x in l]
+        the_boxes.append(z[i])
