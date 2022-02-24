27361b12af72b7885a06358f50ec250538c3ea74
nadiamason <89014605+nadiamason@users.noreply.github.com>
2022-02-24 00:37:00 +0000

    need to debug and work out why iterative function is not solving the sudoku - producing identical grids and therefore stuck in loop. Maybe look at boxes - not sure its working correctly

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,22 +1,88 @@
+import numpy as np
+
 def divide_chunks(l, n):
       
     # looping till length l
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
-def making_grid(l, n):
-    print(l)
+def solving_iteration(grid, columns, the_boxes, og):
 
+    for row in grid:
 
-grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 0, 0, 3, 1],
-        [0, 0, 3, 0, 1, 0, 0, 8, 0],
-        [9, 0, 0, 8, 6, 3, 0, 0, 5],
-        [0, 5, 0, 0, 9, 0, 6, 0, 0],
-        [1, 3, 0, 0, 0, 0, 2, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 7, 4],
-        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+        rownumber = grid.index(row)
+
+        if rownumber <= 2:
+            boxnumber = 0 
+        elif rownumber >= 6:
+            boxnumber = 2
+        else:
+            boxnumber = 3
+
+        for x in row:
+
+            if x == 0:
+
+                columnnumber = row.index(x)
+                if columnnumber > 2 and columnnumber < 6 and boxnumber == 0:
+                    boxnumber = 3
+                elif columnnumber > 2 and columnnumber < 6 and boxnumber == 1:
+                    boxnumber = 4
+                elif columnnumber > 2 and columnnumber < 6 and boxnumber == 2:
+                    boxnumber = 5    
+                elif columnnumber >= 6 and boxnumber == 1:
+                    boxnumber = 6
+                elif columnnumber >= 6 and boxnumber == 2:
+                    boxnumber = 7
+                elif columnnumber >= 6 and boxnumber == 3:
+                    boxnumber = 8 
+
+                value = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                for n in set(grid[rownumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+                
+                for n in set(columns[columnnumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+
+                for n in set(the_boxes[boxnumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+                
+                if len(value) == 1:
+                    grid[rownumber][columnnumber] = value[0]
+    if grid == og:
+        print("Identical grids")
+        exit
+    return grid
+
+#grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+#        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+#        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+#        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+#        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+#        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+#        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+#        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+#        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+grid = [[ 0,0,8,0,0,9,0,0,0],
+        [ 6,2,0,0,7,0,0,0,5],
+        [ 3,0,4,0,0,0,8,0,0],
+        [ 0,0,0,4,2,0,0,0,0],
+        [ 2,0,0,9,1,0,0,8,7],
+        [ 7,9,1,6,0,0,0,0,4],
+        [ 5,8,0,1,3,2,4,0,9],
+        [ 4,3,7,5,9,8,1,6,9],
+        [ 0,0,2,0,0,0,5,0,0]]
 
 columns = []
 
@@ -47,3 +113,14 @@ for box in final_boxes:
     for i in range(0,3):
         z[i] = [x for l in z[i] for x in l]
         the_boxes.append(z[i])
+
+counter = 0
+while any(0 in sl for sl in grid) == True:
+    grid = solving_iteration(grid, columns, the_boxes, grid)
+    print(grid)
+    counter + 1
+    if counter == 100:
+        print("Not working")
+        break
+
+print(the_boxes)
