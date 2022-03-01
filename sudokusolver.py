570330e2ab9ca2afabe7bc4d081526674c74fdd0
nadiamason <89014605+nadiamason@users.noreply.github.com>
2022-03-01 14:07:46 +0000

    making function and half completed finding singles function

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,4 +1,5 @@
-import numpy as np
+from collections import Counter
+from itertools import chain
 
 def divide_chunks(l, n):
       
@@ -40,11 +41,11 @@ def make_columns_boxes(grid):
     return the_boxes, columns
 
 def solving_iteration(grid):
+    rownumber = 0
 
     for row in grid:
 
-        rownumber = grid.index(row)
-
+        columnnumber = 0
         for x in row:
 
             if x == 0:
@@ -54,20 +55,19 @@ def solving_iteration(grid):
                 elif rownumber >= 6:
                     boxnumber = 2
                 else:
-                    boxnumber = 3
+                    boxnumber = 1
 
-                columnnumber = row.index(x)
                 if columnnumber > 2 and columnnumber < 6 and boxnumber == 0:
                     boxnumber = 3
                 elif columnnumber > 2 and columnnumber < 6 and boxnumber == 1:
                     boxnumber = 4
                 elif columnnumber > 2 and columnnumber < 6 and boxnumber == 2:
                     boxnumber = 5    
-                elif columnnumber >= 6 and boxnumber == 1:
+                elif columnnumber >= 6 and boxnumber == 0:
                     boxnumber = 6
-                elif columnnumber >= 6 and boxnumber == 2:
+                elif columnnumber >= 6 and boxnumber == 1:
                     boxnumber = 7
-                elif columnnumber >= 6 and boxnumber == 3:
+                elif columnnumber >= 6 and boxnumber == 2:
                     boxnumber = 8 
 
                 the_boxes, columns = make_columns_boxes(grid)
@@ -94,12 +94,84 @@ def solving_iteration(grid):
                 
                 if len(value) == 1:
                     grid[rownumber][columnnumber] = value[0]
-                    
+
+            columnnumber += 1
+        
+        rownumber += 1
+
     return grid
 
-#grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+def finding_singles(grid):
+    rownumber = 0
+
+    for row in grid:
+        unknowns = []
+
+        columnnumber = 0
+        for x in row:
+
+            if x == 0:
+
+                if rownumber <= 2:
+                    boxnumber = 0 
+                elif rownumber >= 6:
+                    boxnumber = 2
+                else:
+                    boxnumber = 1
+
+                if columnnumber > 2 and columnnumber < 6 and boxnumber == 0:
+                    boxnumber = 3
+                elif columnnumber > 2 and columnnumber < 6 and boxnumber == 1:
+                    boxnumber = 4
+                elif columnnumber > 2 and columnnumber < 6 and boxnumber == 2:
+                    boxnumber = 5    
+                elif columnnumber >= 6 and boxnumber == 0:
+                    boxnumber = 6
+                elif columnnumber >= 6 and boxnumber == 1:
+                    boxnumber = 7
+                elif columnnumber >= 6 and boxnumber == 2:
+                    boxnumber = 8 
+
+                the_boxes, columns = make_columns_boxes(grid)
+
+                value = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                for n in set(grid[rownumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+                
+                for n in set(columns[columnnumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+
+                for n in set(the_boxes[boxnumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+
+                unknowns.append(value)
+            
+            # [[1,9],[2,9],[3,9]]
+                
+            columnnumber += 1
+
+                
+        
+        rownumber += 1
+
+
+
+
+                
+
+#grid = [[3, 0, 6, 5, 7, 8, 4, 0, 0],
 #        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-#        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+#        [4, 8, 7, 6, 2, 9, 5, 3, 1],
 #        [0, 0, 3, 0, 1, 0, 0, 8, 0],
 #        [9, 0, 0, 8, 6, 3, 0, 0, 5],
 #        [0, 5, 0, 0, 9, 0, 6, 0, 0],
@@ -107,25 +179,30 @@ def solving_iteration(grid):
 #        [0, 0, 0, 0, 0, 0, 0, 7, 4],
 #        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
-grid = [[ 0,0,8,0,0,9,0,0,0],
-        [ 6,2,0,0,7,0,0,0,5],
-        [ 3,0,4,0,0,0,8,0,0],
-        [ 0,0,0,4,2,0,0,0,0],
-        [ 2,0,0,9,1,0,0,8,7],
-        [ 7,9,1,6,0,0,0,0,4],
-        [ 5,8,0,1,3,2,4,0,9],
-        [ 4,3,7,5,9,8,1,6,9],
-        [ 0,0,2,0,0,0,5,0,0]]
+grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
 
 
 counter = 0
 while any(0 in sl for sl in grid) == True:
     grid = solving_iteration(grid)
-    print(grid)
-    counter + 1
-    if counter == 100:
-        print("Not working")
+    
+    if counter == 10:
         break
+    
+    counter += 1
+    print(counter)
 
-print(the_boxes)
+grid = finding_singles(grid)
+
+print(counter)
+for row in grid:
+    print(row)
