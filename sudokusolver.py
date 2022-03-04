83a2dc536301cd2d22d3c192b88c5999dd72a777
nadiamason <89014605+nadiamason@users.noreply.github.com>
2022-03-04 16:42:40 +0000

    made function columns singles - messing around a bit more

--- sudokusolver.py
+++ sudokusolver.py
@@ -133,6 +133,7 @@ def finding_singles(grid):
                     boxnumber = 8 
 
                 the_boxes, columns = make_columns_boxes(grid)
+                print(the_boxes)
 
                 value = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -183,7 +184,6 @@ def finding_singles(grid):
                     
             thiscounter += 1
             
-                # now have unknowns = [1,[2,9], [2,9]]
 
         unknowncolumnnumber = 0
         unknowncounter = 0
@@ -196,22 +196,123 @@ def finding_singles(grid):
                 unknowncounter += 1
             unknowncolumnnumber += 1 
 
-
-                
-        
         rownumber += 1
     
     return grid
 
-grid = [[0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 9, 0, 0, 6, 0, 7],
-        [0, 9, 0, 0, 8, 3, 0, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 5, 0, 3, 0, 0, 0, 2, 0],
-        [9, 0, 0, 0, 7, 1, 0, 0, 5],
-        [0, 0, 5, 1, 0, 2, 0, 0, 0],
-        [0, 3, 0, 0, 0, 0, 0, 6, 0],
-        [0, 0, 0, 7, 0, 4, 0, 0, 8]]
+def column_singles(grid):
+
+    columnnumber = 0
+    the_boxes, columns = make_columns_boxes(grid)
+
+    for column in columns:
+        unknowns = []
+
+        rownumber = 0
+        for x in column:
+
+            if x == 0:
+
+                if rownumber <= 2:
+                    boxnumber = 0 
+                elif rownumber >= 6:
+                    boxnumber = 2
+                else:
+                    boxnumber = 1
+
+                if columnnumber > 2 and columnnumber < 6 and boxnumber == 0:
+                    boxnumber = 3
+                elif columnnumber > 2 and columnnumber < 6 and boxnumber == 1:
+                    boxnumber = 4
+                elif columnnumber > 2 and columnnumber < 6 and boxnumber == 2:
+                    boxnumber = 5    
+                elif columnnumber >= 6 and boxnumber == 0:
+                    boxnumber = 6
+                elif columnnumber >= 6 and boxnumber == 1:
+                    boxnumber = 7
+                elif columnnumber >= 6 and boxnumber == 2:
+                    boxnumber = 8 
+
+                
+
+                value = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                for n in set(grid[rownumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+                
+                for n in set(columns[columnnumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+
+                for n in set(the_boxes[boxnumber]):
+                    try:
+                        value.remove(n)
+                    except ValueError:
+                        continue
+
+                unknowns.append(value)
+            rownumber += 1
+            
+        flat_list = [item for sublist in unknowns for item in sublist]
+
+        my_dict = {i:flat_list.count(i) for i in flat_list}
+
+        list_of_key = list(my_dict.keys())
+        list_of_value = list(my_dict.values())
+                
+        thiscounter = 0
+        for i in list_of_value:
+            # value is the number which we will be replacing with        
+            value = list_of_key[thiscounter]
+            # i is the number of duplicates = if 1 can replace it
+            if i == 1:
+                index = 0
+                for lists in unknowns:
+                    try:
+                        if value in lists:
+                            unknowns[index] = value
+                    except TypeError:
+                        index += 1
+                        continue
+
+                    index += 1
+                    
+            thiscounter += 1
+            
+
+        unknownrownumber = 0
+        unknowncounter = 0
+        for x in column:
+            
+            # index of the grid is going to be wrong here....
+            if x == 0:
+                if type(unknowns[unknowncounter]) == int:
+                    grid[unknownrownumber][columnnumber] = unknowns[unknowncounter]
+                unknowncounter += 1
+            unknownrownumber += 1 
+
+        columnnumber += 1
+    
+    return grid
+
+
+
+
+grid = [[1, 0, 0, 0, 0, 5, 0, 7, 0],
+        [2, 0, 5, 0, 0, 0, 4, 0, 0],
+        [0, 0, 0, 6, 0, 0, 8, 0, 0],
+        [0, 0, 7, 0, 0, 0, 0, 1, 9],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 9, 0, 0, 0, 8, 0, 0, 6],
+        [0, 0, 0, 1, 9, 6, 0, 0, 0],
+        [7, 4, 0, 0, 8, 0, 0, 0, 0],
+        [3, 0, 0, 0, 7, 0, 0, 0, 0]]
+
 
 
 
@@ -223,29 +324,25 @@ while any(0 in sl for sl in grid) == True:
         break
     
     counter += 1
-    print(counter)
+    print("First loop")
 
-grid = finding_singles(grid)
 
+
+counter = 0
 while any(0 in sl for sl in grid) == True:
     grid = solving_iteration(grid)
+    grid = finding_singles(grid)
+    grid = column_singles(grid)
     
     if counter == 17:
         break
-    
     counter += 1
-    print(counter)
 
 grid = finding_singles(grid)
 
-while any(0 in sl for sl in grid) == True:
-    grid = solving_iteration(grid)
-    
-    if counter == 17:
-        break
-    
-    counter += 1
-    print(counter)
+
+grid = solving_iteration(grid)
 
 for row in grid:
     print(row)
+
